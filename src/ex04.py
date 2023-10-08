3bcae7c05413fb2ed6dee8d7f90dadf67841f43e
kittinun <6056009@kmitl.ac.th>
2023-10-08 11:24:48 +0700

    FIX test volume

--- src/ex04.py
+++ src/ex04.py
@@ -20,18 +20,9 @@ def perimeter(param1, param2):
 
 
 def volume(param1, param2, param3):
-    """
-    Calculates the volume of an object given its length, width, and height.
-
-    Args:
-        length (float): The length of the object.
-        width (float): The width of the object.
-        height (float): The height of the object.
-
-    Returns:
-        float: The volume of the object.
-    """
-    # TODO : complete this
+    volume = (param1*param2)*param3
+    return volume
+    # FIX : complete this
     pass
 
 
