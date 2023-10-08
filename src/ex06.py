705c35d06b05b195a17b6999f813824c86f55ee2
kittinun <6056009@kmitl.ac.th>
2023-10-08 12:13:04 +0700

    FIX test ordinal suffix

--- src/ex06.py
+++ src/ex06.py
@@ -3,15 +3,21 @@ Execise 6
 """
 
 
-def ordinal_suffix(param):
-    """
-    Return the ordinal suffix for a given number.
+def ordinal_suffix(num):
+    # Handle special cases: 11th, 12th, 13th
+    if 10 < num % 100 < 14:
+        return str(num) + 'th'
 
-    Parameters:
-        num (int): The number for which to find the ordinal suffix.
+    # Handle general cases
+    last_digit = num % 10
+    if last_digit == 1:
+        return str(num) + 'st'
+    elif last_digit == 2:
+        return str(num) + 'nd'
+    elif last_digit == 3:
+        return str(num) + 'rd'
+    else:
+        return str(num) + 'th'
 
-    Returns:
-        str: The ordinal suffix corresponding to the given number.
-    """
-    # TODO : complete this
+    # FIX : complete this
     pass
