6425a198014fa3cd5dec40b5cc7d88748ab5ea33
kittinun <6056009@kmitl.ac.th>
2023-10-08 11:09:50 +0700

    FIX test is odd

--- src/ex03.py
+++ src/ex03.py
@@ -4,25 +4,20 @@ Execise 3
 
 
 def is_odd(num):
-     nums = int(num)
-    if nums % 2 != 0:
+    if num % 2 == 1:
         return True
     else:
         return False
-    """
-    Check if a number is odd.
 
-    Args:
-        num (int): The number to be checked.
-
-    Returns:
-        bool: True if the number is odd, False otherwise.
-    """
-    # TODO : complete this
+    # FIX : complete this
     pass
 
 
 def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
     """
     Determines if a given number is even.
 
@@ -32,5 +27,5 @@ def is_even(num):
     Returns:
         bool: True if the number is even, False otherwise.
     """
-    # TODO : complete this
+    # FIX : complete this
     pass
