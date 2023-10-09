cbafe9317aca38878c89b951e8539fe2d3746f0e
kittinun <6056009@kmitl.ac.th>
2023-10-09 21:59:05 +0700

    FIX test 13 cal sum cal product

--- src/ex13.py
+++ src/ex13.py
@@ -1,11 +1,32 @@
-def test_calc_sum(numbers):
-        return sum(numbers)
+def calc_sum(num_list):
+    """
+    Calculate the sum of a list of numbers.
 
-def test_calc_prod(numbers):
-    if not numbers:  # If the list is empty, return 1
-        return 1
-    result = 1
-    for number in numbers:
-        result *= number
-    return result
+    Parameters:
+    - num_list (list): A list of numbers.
 
+    Returns:
+    - int: The sum of all the numbers in the list.
+    """
+    count = 0
+    for i in num_list:
+        count = count + i
+
+    return count
+
+
+def calc_prod(num_list):
+    """
+    Calculates the product of all the numbers in the given list.
+
+    Parameters:
+        num_list (list): A list of numbers.
+
+    Returns:
+        int: The product of all the numbers in the list.
+    """
+    count = 1
+    for i in num_list:
+        count = count * i
+
+    return count
