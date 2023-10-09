c05359cab64de92be7c4f8492e36779a516860d6
kittinun <6056009@kmitl.ac.th>
2023-10-09 21:37:53 +0700

    FIX test 11 get_hr_min_sec

--- src/ex07.py
+++ src/ex07.py
@@ -1,19 +1,12 @@
 """
 Execise 7
-"""
+"""def printASCIITable():
+    # Loop over integers 32 up to and including 126:
+    for i in range(32, 127):  # Fill in the blanks
+        # Print the integer and its ASCII text character:
+        print(i, chr(i))      # Fill in the blanks
+
+printASCIITable()
 
 
-def print_ASCII_table():
-    """
-    Generate and print the ASCII table for a range of characters.
-
-    Args:
-        start_char (int): The ASCII value of the starting character.
-        end_char (int): The ASCII value of the ending character.
-
-    Returns:
-        str: Returns nothing. The function simply prints the ASCII characters.
-
-    """
-    # TODO : complete this
-    pass
+# TODO : complete this
--- src/ex11.py
+++ src/ex11.py
@@ -1,25 +1,52 @@
-"""
-Exercise 11
-"""
-
-
-def get_hr_min_sec(param):
+def get_hr_min_sec(totalSeconds):
     """
     Generates a string representation of the given number
     of seconds in the format "9h 9m 9s".
 
     Args:
-        tsec (int): The number of seconds to convert to
+        totalSeconds (int): The number of seconds to convert to
         the "9h 9m 9s" format. Default is 0s.
 
     Returns:
         str: A string representation of the given
         number of seconds in the "9h 9m 9s" format.
-
-    Example:
-        >>> get_hr_min_sec(3665)
-        '1h 1m 5s'
-        >>> get_hr_min_sec(0)
-        '0s'
     """
-    pass
+
+    # If totalSeconds is 0, just return '0s':
+    if totalSeconds == 0:
+        return '0s'
+
+    # Set hours to 0, then add an hour for every 3600 seconds removed from
+    # totalSeconds until totalSeconds is less than 3600:
+    hours = 0
+    while totalSeconds >= 3600:
+        hours += 1
+        totalSeconds -= 3600
+
+    # Set minutes to 0, then add a minute for every 60 seconds removed from
+    # totalSeconds until totalSeconds is less than 60:
+    minutes = 0
+    while totalSeconds >= 60:
+        minutes += 1
+        totalSeconds -= 60
+
+    # Set seconds to the remaining totalSeconds value:
+    seconds = totalSeconds
+
+    # Create an hms list that contains the string hour/minute/second amounts:
+    hms = []
+
+    # If there are one or more hours, add the amount with an 'h' suffix:
+    if hours > 0:
+        hms.append(str(hours) + 'h')
+
+    # If there are one or more minutes, add the amount with an 'm' suffix:
+    if minutes > 0:
+        hms.append(str(minutes) + 'm')
+
+    # If there are one or more seconds, add the amount with an 's' suffix:
+    if seconds > 0:
+        hms.append(str(seconds) + 's')
+
+    # Join the hour/minute/second strings with a space in between them:
+    return ' '.join(hms)
