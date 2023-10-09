0d39d272a329791eb360d118a15ea169bc08d032
kittinun <6056009@kmitl.ac.th>
2023-10-09 22:29:38 +0700

    FIX test 17 roll dice

--- src/ex17.py
+++ src/ex17.py
@@ -1,10 +1,6 @@
-"""
-Exercise 17
-"""
 import random
 
-
-def roll_dice(params):
+def roll_dice(num_of_dice):
     """
     Calculate the total sum of rolling a certain number of dice.
 
@@ -14,4 +10,15 @@ def roll_dice(params):
     Returns:
         int: The total sum of the dice rolls.
     """
-    pass
+
+    # Start the sum total at 0:
+    total = 0
+
+    # Run a loop for each die that needs to be rolled:
+    for i in range(num_of_dice):
+        # Add the amount from one 6-sided dice roll to the total:
+        total += random.randint(1, 6)
+
+    # Return the dice roll total:
+    return total
+#fix to this
