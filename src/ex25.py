aa8c5ee2504c955bb8f63c2b42a8681b42ec3103
kittinun <6056009@kmitl.ac.th>
2023-10-11 22:49:17 +0700

    FIX test25 multiplication table

--- src/ex25.py
+++ src/ex25.py
@@ -15,7 +15,7 @@ Exercise 25 : Multiplication Table
 """
 
 
-def multiplication_table(param, param1 = 11):
+def multiplication_table(start, end=11):
     """
     Generates a multiplication table from the given start to the end number.
 
@@ -28,4 +28,13 @@ def multiplication_table(param, param1 = 11):
     """
     print('  | 1  2  3  4  5  6  7  8  9 10')
     print('--+------------------------------')
-    # TODO : complete this
+    count = 10
+    for i in range(start, end):
+        result = ' '
+        for j in range(count):
+            result += '{:2d}'.format((j + 1) * i)+' '
+        result = '{:2d}'.format(i)+'|'+result
+        print(result)
+    # Fix : complete this
+
+# %%
