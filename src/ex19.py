c69e43610813aa1e5143216463d421345e67622f
kittinun <6056009@kmitl.ac.th>
2023-10-11 21:34:06 +0700

    FIX test 19 gen password

--- src/ex19.py
+++ src/ex19.py
@@ -1,16 +1,21 @@
-"""
-Exercise 19 : Password Generator
-"""
 import random
 
-LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'    # 26
-UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'    # 26
-NUMBERS = '1234567890'                          # 10
-SPECIAL = '~!@#$%^&*()_+'                       # 13
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'  # 26
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # 26
+NUMBERS = '1234567890'  # 10
+SPECIAL = '~!@#$%^&*()_+'  # 13
 
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
-    # TODO : complete this
-    pass
+def gen_password(leng):
+    # Fix : complete this
+    my_length = leng
+    if leng < 12:
+        my_length = 12
+    my_pass = [LOWER_LETTERS[random.randint(0, 25)], UPPER_LETTERS[random.randint(0, 25)],
+               NUMBERS[random.randint(0, 9)], SPECIAL[random.randint(0, 12)]]
+    while len(my_pass) < my_length:
+        my_pass.append(ALL_CHARS[random.randint(0, 74)])
+    random.shuffle(my_pass)
+    return ''.join(my_pass)
