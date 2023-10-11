3c2d80f9cafea52e1f460ba172b205a1909bfc19
kittinun <6056009@kmitl.ac.th>
2023-10-11 22:54:17 +0700

    FIX test 18 get cost of coffee

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,14 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
+def get_cost_of_coffee(amount, price):
+    free = amount // 9
+    paid = amount - free
+    return paid * price
+    pass
+
+def get_cost_of_coffee_2(count, price):
+    free = count // 9
+    paid = count - free
+    return paid * price
     pass
