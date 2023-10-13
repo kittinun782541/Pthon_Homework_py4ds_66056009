db36ae044bcbc62e8a68c36c84338135715fbeef
kittinun <6056009@kmitl.ac.th>
2023-10-13 20:45:03 +0700

    FIX test  7 ascii table

--- src/ex07.py
+++ src/ex07.py
@@ -1,12 +1,20 @@
 """
 Execise 7
-"""def printASCIITable():
-    # Loop over integers 32 up to and including 126:
-    for i in range(32, 127):  # Fill in the blanks
-        # Print the integer and its ASCII text character:
-        print(i, chr(i))      # Fill in the blanks
-
-printASCIITable()
+"""
 
 
-# TODO : complete this
+def print_ASCII_table(start_char, end_char):
+    """
+    Generate and print the ASCII table for a range of characters.
+
+    Args:
+        start_char (int): The ASCII value of the starting character.
+        end_char (int): The ASCII value of the ending character.
+
+    Returns:
+        str: Returns nothing. The function simply prints the ASCII characters.
+
+    """
+    # Fix : complete this
+    for i in range(start_char, end_char+1):
+        print(chr(i))
